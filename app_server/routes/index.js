e5a19d33efc74230f54c6ef8181c196d55065e01
congyh <yihao.cong@outlook.com>
2016-12-09 21:31:35 +0800

    add locations-list jade

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var ctrlLocations = require("../controller/locations")
-var ctrlOthers = require("../controller/others")
+var ctrlLocations = require("../controller/locations");
+var ctrlOthers = require("../controller/others");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render("index", {"title": "Express"});
-});
+router.get('/', ctrlLocations.homelist);
 router.get("/location", ctrlLocations.locationInfo);
-router.get("/location/review/new", ctrlLocations.addReview)
-router.get("/about", ctrlOthers.about)
+router.get("/location/review/new", ctrlLocations.addReview);
+router.get("/about", ctrlOthers.about);
 
 module.exports = router;
