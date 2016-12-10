4644f12651639240e270248d638d468816ea84c0
congyh <yihao.cong@outlook.com>
2016-12-10 17:49:03 +0800

    [WIP]mongoose connection

---
+++ app_server/models/db.js
@@ -0,0 +1,17 @@
+/**
+ * Created by yihao.cong@outlook.com on 16-12-10.
+ */
+var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://localhost/Loc8r';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var ctrlLocations = require("../controller/locations");
-var ctrlOthers = require("../controller/others");
+var ctrlLocations = require("../controllers/locations");
+var ctrlOthers = require("../controllers/others");
 
 /* GET home page. */
 router.get('/', ctrlLocations.homelist);
